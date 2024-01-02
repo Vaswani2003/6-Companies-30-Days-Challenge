db7ec23d3e353de7c3e3295d74563ce6b4bed23f
Vinamra Vaswani <100992956+Vaswani2003@users.noreply.github.com>
2024-01-02 20:56:27 +0530

    Create Query_Kth_smallest_trimmed_number.cpp

---
+++ Query_Kth_smallest_trimmed_number.cpp
@@ -0,0 +1,43 @@
+/*
+2343. Query Kth Smallest Trimmed Number
+
+You are given a 0-indexed array of strings nums, where each string is of equal length and consists of only digits.
+
+You are also given a 0-indexed 2D integer array queries where queries[i] = [ki, trimi]. For each queries[i], you need to:
+
+Trim each number in nums to its rightmost trimi digits.
+Determine the index of the kith smallest trimmed number in nums. If two trimmed numbers are equal, the number with the lower index is considered to be smaller.
+Reset each number in nums to its original length.
+Return an array answer of the same length as queries, where answer[i] is the answer to the ith query.
+
+Note:
+
+To trim to the rightmost x digits means to keep removing the leftmost digit, until only x digits remain.
+Strings in nums may contain leading zeros.
+*/
+
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> res;
+        int n = nums.size();
+        int m = nums[0].length();
+        int k, trim, i;
+        string str;
+
+        for(auto query: queries){
+            k = query[0];
+            trim = query[1];
+            priority_queue<pair<string,int>> pq;
+
+            for(i=0;i<n;i++){
+                str = nums[i].substr(m-trim);
+                pq.push({str,i});
+
+                if (pq.size() > k)  pq.pop();
+            }
+            res.push_back(pq.top().second);
+        }
+        return res;
+    }
+};
